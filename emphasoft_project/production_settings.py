ae20188933c415402844e5ab5bcec65170f187f5
AlbertFarhutdinov <45900561+AlbertFarhutdinov@users.noreply.github.com>
2020-04-17 18:40:24 +0300

    Update production_settings.py

--- emphasoft_project/production_settings.py
+++ emphasoft_project/production_settings.py
@@ -31,4 +31,4 @@ MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
