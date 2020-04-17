31c53c75cc60ac725103b90eaa7543daa1b5ae2d
AlbertFarhutdinov <45900561+AlbertFarhutdinov@users.noreply.github.com>
2020-04-17 16:49:37 +0300

    Update settings.py

--- emphasoft_project/settings.py
+++ emphasoft_project/settings.py
@@ -130,9 +130,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, os.environ.get('STATICFILES_DIR', 'staticfiles'))
-]
 
 # Social Auth
 
