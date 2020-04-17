531c0b2e7f188023fb96ee98c8c0a5324d0c2de0
AlbertFarhutdinov <45900561+AlbertFarhutdinov@users.noreply.github.com>
2020-04-17 16:40:00 +0300

    Update settings.py

--- emphasoft_project/settings.py
+++ emphasoft_project/settings.py
@@ -129,10 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = os.environ.get('STATICFILES_DIR', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, STATICFILES_DIR)
+    os.path.join(BASE_DIR, os.environ.get('STATICFILES_DIR', 'staticfiles'))
 ]
 
 # Social Auth
